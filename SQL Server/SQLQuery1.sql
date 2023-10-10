e3850c9a8efe963ae2824250cd77204db5bb60b4
Prepsle <phong16203@gmail.com>
2023-10-10 15:13:14 +0700

    Bai-th3

---
+++ SQL Server/SQLQuery1.sql
@@ -0,0 +1,28 @@
+use Startup
+
+create table Menu (
+MenuID int identity(1,1)primary key,
+MenuName nvarchar(50),
+IsActive bit,
+ControllerName nvarchar(50),
+ActiveName nvarchar(50),
+Levels int,
+ParentID int,
+Link nvarchar(50),
+MenuOrder int,
+Position int );
+
+create table Post (
+PostID bigint identity(1,1) primary key,
+Title nvarchar(50),
+Abstract nvarchar(50),
+Contents ntext,
+Images nvarchar(200),
+Link nvarchar(200),
+Author nvarchar(30),
+CreatedDate datetime,
+IsActive bit,
+PostOrder int,
+MenuID int,
+Category int,
+Status int );
---
+++ WebApplication1/Components/PostViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Components
+{
+    [ViewComponent(Name = "Post")]
+    public class PostViewComponent : ViewComponent
+    {
+        private DataContext _context;
+        public PostViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofMenu = (from m in _context.Posts
+                              where (m.IsActive == true) && (m.Status == 1)
+                              select m).ToList();
+            //m.Position == 1 menu nằm trên
+            //m.Position == 0 menu nằm dưới
+
+            return await Task.FromResult((IViewComponentResult)View("Default", listofMenu));
+        }
+    }
+}
